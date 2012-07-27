597a52b69f820b394ff5fa76c54a08e5295d8036
Jairard <gaetan_champarnaud@hotmail.com>
2012-07-27 11:18:14 +0200

    Déplacement des sources dans le dossier /src, modification du fichier /build/premake4.lua en conséquence

--- build/premake4.lua
+++ build/premake4.lua
@@ -1,16 +1,12 @@
 -- A solution contains projects, and defines the available configurations
 solution "kinect"
     configurations "Release"
-	files { "../**.h", "../**.cpp" }
 	links = iif (os.is("windows"), { "pthread", "ws2_32" }, "pthread")
 
 	-- A project defines one build target
-	project "kinect_test"
-		kind "ConsoleApp"
-		language "C++"
-
 	project "kinect"
-		--kind "StaticLib"
-		kind "SharedLib"
+		kind "StaticLib"
+		--kind "SharedLib"
 		language "C++"
-		--excludes { "../main.cpp", "../wave.cpp", "../wave.h" }
+		files { "../src/**.h", "../src/**.cpp" }
+		excludes "../src/main.cpp" 
